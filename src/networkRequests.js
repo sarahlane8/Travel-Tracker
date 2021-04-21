334fa027832684f2296588f8cf3099fd358ddb35
Sarah Lane <sarahlane8@gmail.com>
2021-04-21 13:49:02 -0600

    Add fetch requests to get all travelers, all trips, and all desinations

---
+++ src/networkRequests.js
@@ -0,0 +1,41 @@
+const fetchData = () => {
+  const travelersData = fetch('http://localhost:3001/api/v1/travelers')
+    .then(response => response.json())
+    .then(travelersData => {
+      return traelversData;
+    })
+
+
+    // const singleTravelerData
+
+  const tripsData = fetch('http://localhost:3001/api/v1/trips')
+    .then(response => response.json())
+    .then(tripsData => {
+      return tripsData;
+    })
+
+
+  const destinationsData = fetch('http://localhost:3001/api/v1/destinations')
+    .then(response => response.json())
+    .then(destinationsData => {
+      return destaintionsData;
+    })
+
+    return Promise.all([travelersData, tripsData, destinationsData])
+      .then(data => {
+      const allData = {};
+      allData.travelersData= data[0];
+      allData.tripsData = data[1];
+      allData.destinationsData = data[2];
+      return allData;
+  })
+  .catch(err => console.log('ERROR', err))
+
+}
+
+
+
+
+
+
+export { fetchData };
