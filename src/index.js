4c7efc8dedb05c8dfc54d71c857c6c147849234a
Sarah Lane <sarahlane8@gmail.com>
2021-04-21 16:09:33 -0600

    Add imports to index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import domUpdates from './domUpdates.js'
 
+import { fetchAllData, fetchSingleTravelerData, addNewTrip, addNewDestination } from './networkRequests.js'
+import Travler from './traveler.js'
+import Trip from './trip.js'
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
