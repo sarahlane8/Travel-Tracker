02557e7f4e0aa2a37f049c02fadffe0b876f5d19
Sarah Lane <sarahlane8@gmail.com>
2021-04-22 08:34:24 -0600

    Move function for later use to scripts.js

---
+++ src/scripts.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+// create a function to combine data sets so that trips also has estimated cost per day and estimated flight cost per person
+
+
+
+// filterMyTrips(trips) {//move to stripts
+//   const myTrips = trips.filter(trip => trip.userID === this.id)
+//   this.myTrips.push(...myTrips)
+//   this.sortMyTrips(this.myTrips)
+// }
