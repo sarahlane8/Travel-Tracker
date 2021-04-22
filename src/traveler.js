5c8973ae6c78ed10137c9eb38e31a62302320a8a
Sarah Lane <sarahlane8@gmail.com>
2021-04-22 05:12:15 -0600

    Add filterMyTrips method and test

--- src/traveler.js
+++ src/traveler.js
@@ -1,56 +1,24 @@
+// import Trip from '..src/Trip.js'//*************
+
 class Traveler {
-  constructor(travelerInfo, trips) {//info for 1 person  filter trips before passing them through?
+  constructor(travelerInfo) {//info for 1 person  filter trips before passing them through?
     this.myTravelInfo = travelerInfo;//do I need this property?
     this.id = travelerInfo.id;
     this.name = travelerInfo.name;
     this.travelerType = travelerInfo.travelerType;
     this.myTrips = [];//do I need this?  list of all trips for 1 travler, take it out of constructor
-    //start testing from line 7 on
-    this.myCurrentTrip;
-    this.myPastTrips = [];
-    this.myFutureTrips = [];
-    this.myPendingTrips = [];
+
   }
+
+  filterMyTrips(trips) {
+    const myTrips = trips.filter(trip => trip.userID === this.id)
+    this.myTrips.push(...myTrips)
+  }
+
+
+
 }
 
-filterMyTrips() {
-  //
-}
-
-sortMyTrips() {
-  //is this already sorted
-  // sort trips by status and date, push them to appropriate properties
-}
-
-calculateTotalCostThisYear() {//is this per calendar year?
-//iterate over my trips, need to access the destinations (for cost per day and flights ) and trips array (for number of travelers, duration, and need to match destination ID to destinationsID)
-}
-
-calculateCostOfTrip() {
-  
-}
-//method to filter all trips by 1 user to display all their trips (regardless of trip status)
-//method to calculate total cost spent on trips + 10% of that total
-//method to calculate cost of a proposed trip + 10
 
 
 export default Traveler;
-
-
-
-class Trip {
-  constructor(tripData) {//1 trip?? or all trips?
-    this.tripData = tripsData//do I need this?
-    this.id = tripData.id;
-    this.userID = tripData.userID;
-    this.destinationID = tripData.destinationID;
-    this.travelers = tripData.travelers;
-    this.date = tripData.date;//use day js here at all?
-    this.duration = tripData.duration;
-    this.status = tripData.status;
-    this.suggestedActivities = tripData.suggestedActivities;
-  }
-}
-
-// consider using dayJS to see set an end date from the start date based on duration
-// see if the trip is currently going on
--- test/traveler-test.js
+++ test/traveler-test.js
@@ -1,12 +1,13 @@
 import { expect } from 'chai';
 import { travelers } from './testData.js'//CHANGE THIS ONE!
+import { trips } from './testData.js'
 import Traveler from '../src/traveler.js'
 
 let traveler;
 
 describe('Traveler', () => {
   beforeEach(() => {
-    traveler = new Traveler(travelers[0])
+    traveler = new Traveler(travelers[0], )
   });
 
   it('should have an object of my data', () => {
@@ -24,6 +25,10 @@ describe('Traveler', () => {
   it('should have a property of travelerType', () => {
     expect(traveler.travelerType).to.equal('relaxer');
   })
-  it('should return a list of my ')
+
+  it('should return a list of my trips based on a userID', () => {
+    traveler.filterMyTrips(trips);
+    expect(traveler.myTrips.length).to.equal(4);
+  })
 
 });
