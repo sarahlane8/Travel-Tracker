6255c182417fe5919e7f8a7125f41def7a78c206
Sarah Lane <sarahlane8@gmail.com>
2021-04-24 05:33:24 -0600

    Add color variables to Sass

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,6 @@
 import Traveler from './traveler.js';//do i need this here?
 import Trip from './trip.js';
 import domUpdates from './domUpdates.js'
-<<<<<<< HEAD
 import { fetchAllData, fetchSingleTravelerData, addNewTrip, addNewDestination } from './networkRequests'
 
 //change traveler later based on the log in page
@@ -9,15 +8,6 @@ import { fetchAllData, fetchSingleTravelerData, addNewTrip, addNewDestination } 
 //*******MEDIA QUERIES********//
 
 let travelers, trips, destinations, currentTraveler;
-=======
-import { fetchAllData, fetchSingleTravelerData, addNewTrip, addNewDestination } from './networkRequests.js'
-
-
-
-//*******MEDIA QUERIES********//
-
-let travelers, trips, destinations;
->>>>>>> main
 
 //*******Event Listeners******//
 
@@ -25,7 +15,6 @@ window.onload = onPageLoad();
 
 
 function onPageLoad() {
-<<<<<<< HEAD
   fetchAllData(25)
   .then(allData => {
     // console.log(allData)
@@ -46,16 +35,6 @@ function onPageLoad() {
   // console.log(travelerInfo.id)
 
 
-=======
-  fetchAllData()
-  .then(allData => {
-    travelers = allData.travelersData;
-    trips = allData.tripsData;
-    destinations = allData.destinationsData;
-    combineDataSets(trips, destinations);
-  })
-}
->>>>>>> main
 
 function combineDataSets(tripData, destinationData) {
   const result = tripData.trips.map(trip => {
