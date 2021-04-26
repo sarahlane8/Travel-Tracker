80e62b79cf0ea5c57b8b6034c97b6a0f5c55e9e5
Sarah Lane <sarahlane8@gmail.com>
2021-04-26 11:41:21 -0600

    Add another test to traveler-test and fix non-working tests

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -2,14 +2,26 @@ import { expect } from 'chai';
 import { travelers, trips } from './testData.js'
 import Traveler from '../src/traveler.js'
 
-let traveler;
-let myTrips;
+let traveler, myTrips, trip;
 
 describe('Traveler', () => {
   beforeEach(() => {
     traveler = new Traveler(travelers[0]);
     myTrips = trips.filter(trip => trip.userID === traveler.id);
     traveler.myTrips = myTrips;
+    trip = {
+      id: 206,
+      userID: 15,
+      destinationID: 18,
+      travelers: 2,
+      date: '2021/12/05',
+      duration: 10,
+      status: 'pending',
+      suggestedActivities: [],
+      estimatedLodgingCostPerDay: 100,
+      estimatedFlightCostPerPerson: 930
+    };
+
   })
 
   it('should have a property of ID', () => {
@@ -37,21 +49,21 @@ describe('Traveler', () => {
 
   it('should sort my trips by past, present, future, and pending trips', () => {
     traveler.sortMyTrips(traveler.myTrips)
-    expect(traveler.myCurrentTrip).to.deep.equal(trips[0]);
-    expect(traveler.myPastTrips.length).to.equal(0);
+    expect(traveler.myCurrentTrip).to.deep.equal(undefined);
+    expect(traveler.myPastTrips.length).to.equal(1);
     expect(traveler.myFutureTrips.length).to.equal(1);
     expect(traveler.myPendingTrips.length).to.equal(2);
   })
 
   it('should calculate how much a traveler has spend on their trips in the past year', () => {
-    expect(traveler.calculateSpentOnTripsThisYear('2021/04/22')).to.equal(13475)
+    expect(traveler.calculateSpentOnTripsThisYear('2021/04/28')).to.equal("You spent $13475.00 on trips in the last year!")
   })
 
   it('should calculate cost in last year for different travelers', () => {
     const traveler3 = new Traveler(travelers[6])
     myTrips = trips.filter(trip => trip.userID === traveler3.id);
     traveler3.sortMyTrips(myTrips)
-    expect(traveler3.calculateSpentOnTripsThisYear('2021/04/22')).to.equal(11682)
+    expect(traveler3.calculateSpentOnTripsThisYear('2021/04/28')).to.equal("You spent $11682.00 on trips in the last year!")
   })
 
   it('should return a string if a traveler hasn\'t traveled in the last 12 months', () => {
@@ -61,6 +73,11 @@ describe('Traveler', () => {
     traveler2.calculateSpentOnTripsThisYear('2021/04/22')
     expect(traveler2.calculateSpentOnTripsThisYear('2021/04/22'))
       .to.equal("You haven't traveled with us recently! We'd love to help you book your next trip!")
+  })
+
+  it('should be able to add a trip to a user\'s myTrips property', () => {
+    traveler.addTrip(trip);
+    expect(traveler.myTrips.length).to.equal(5);
   })
 
 });
